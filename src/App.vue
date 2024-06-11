7ff3abae44d41f4eaabcfb14dba77c21bf4a8d1a
Jessica <jessicaennor@gmail.com>
2024-06-11 23:33:42 +0200

    Created a housing booking app using vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <Home />
+  <h1> Find Your Home Away From Home
+             <br>  AND  <br>
+        Discover Comfort Anywhere</h1>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './components/Home.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Home,
+  },
+      
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app { 
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+color: #e5e5e5;
+margin: 50px auto; /* Change this to center the element horizontally */
+background-image: url("./assets/istockphoto-1140134578-612x612.jpg");
+background-size: cover;
+height: 70vh;
+background-repeat: no-repeat;
+/* display: flex; */
+align-items: center;
+justify-content: center;
+max-width: 1000px;
+background-color: rgba(0, 0, 0, 0.5);
+background-blend-mode: multiply;
 }
+
+img{
+  height: 40px;
+}
+h1{
+  text-align: center;
+  font-size: 30px;
+}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <div class="logo">
+      <img alt="Housing logo" src="../assets/file (1).png" />
+      <h4 style=" margin-left: 30px; margin-top:0%; font-size: 18px">
+        NestQuest <br />
+        <span style="font-size: 13px; margin-left: 20px">Housing</span>
+      </h4>
+    </div>
+
+      <a href="#">Home</a>
+      <a href="#">Landlord</a>
+      <a href="#">Tenants</a>
+      <a href="#">Contact Us</a>
+    
+    
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HomeComponent",
+  
+};
+</script>
+
+<style scoped>
+header {
+    margin-top: 20px;
+  display: inline-flex;
+}
+a {
+  color: #e5e5e5;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  margin-right: 25px;
+  margin-left: 20px;
+}
+.logo {
+  margin-right: 350px;
+}
+
+</style>
